ea902b70d4c183ccaf5947ea07e74712dbdb98f1
Jordan-Maloney <79399578+Jordan-Maloney@users.noreply.github.com>
2021-03-31 11:53:10 +1300

    Add files via upload

---
+++ chessboard.py
@@ -0,0 +1,11 @@
+##
+# chessboard.py
+# Jordan Maloney
+# 31/3/21
+
+def chessboard():
+    """
+    Print a list of all chessboard values from A1 to H8
+    """
+    tiles = [["A", "B", "C", "D", "E", "F", "G", "H"] [8, 7, 6, 5, 4, 3, 2, 1]]
+    
