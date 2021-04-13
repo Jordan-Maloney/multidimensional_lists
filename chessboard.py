025f87613ffc2aeb63cd071b1823de7de7700d7e
Jordan-Maloney <79399578+Jordan-Maloney@users.noreply.github.com>
2021-04-14 11:42:00 +1200

    Update chessboard.py

--- chessboard.py
+++ chessboard.py
@@ -7,5 +7,23 @@ def chessboard():
     """
     Print a list of all chessboard values from A1 to H8
     """
-    tiles = [["A", "B", "C", "D", "E", "F", "G", "H"] [8, 7, 6, 5, 4, 3, 2, 1]]
-    
+    row = []
+    MAX_SIZE = 8
+    for i in range(MAX_SIZE):
+        col = []
+        row.append(col)
+        for j in range(MAX_SIZE):
+            row[i].append((i, j))
+    return row
+
+def display_board(board):
+    ROW = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            print(ROW[j] + str(8-i), end = " ")
+        print()
+
+# Main routine
+if __name__ == "__main__":
+    board = chessboard()
+    display_board(board)
